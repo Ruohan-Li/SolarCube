c07fcbd1ba1b6ff3b68a36c59d3f27f48442e196
Ruohan-Li <lir@gsapp6.gshpc.umd.edu>
2024-07-05 17:07:01 -0400

    clear

--- model/informer_transformer/main_transformer.py
+++ model/informer_transformer/main_transformer.py
@@ -70,21 +70,6 @@ if args.use_gpu and args.use_multi_gpu:
     args.device_ids = [int(id_) for id_ in device_ids]
     args.gpu = args.device_ids[0]
 
-# data_parser = {
-#     'ETTh1':{'data':'ETTh1.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
-#     'ETTh2':{'data':'ETTh2.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
-#     'ETTm1':{'data':'ETTm1.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
-#     'ETTm2':{'data':'ETTm2.csv','T':'OT','M':[7,7,7],'S':[1,1,1],'MS':[7,7,1]},
-#     'WTH':{'data':'WTH.csv','T':'WetBulbCelsius','M':[12,12,12],'S':[1,1,1],'MS':[12,12,1]},
-#     'ECL':{'data':'ECL.csv','T':'MT_320','M':[321,321,321],'S':[1,1,1],'MS':[321,321,1]},
-#     'Solar':{'data':'solar_AL.csv','T':'POWER_136','M':[137,137,137],'S':[1,1,1],'MS':[137,137,1]},
-# }
-# if args.data in data_parser.keys():
-#     data_info = data_parser[args.data]
-#     args.data_path = data_info['data']
-#     args.target = data_info['T']
-#     args.enc_in, args.dec_in, args.c_out = data_info[args.features]
-
 args.s_layers = [int(s_l) for s_l in args.s_layers.replace(' ','').split(',')]
 args.detail_freq = args.freq
 args.freq = args.freq[-1:]
